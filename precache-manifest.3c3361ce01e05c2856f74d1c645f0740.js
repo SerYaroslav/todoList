a3318fd4005db632aafe3025628ace1ed63626f4
Yaroslav Serediuk <seredyukua@gmail.com>
2020-07-05 22:03:24 +0300

    Updates

---
+++ precache-manifest.3c3361ce01e05c2856f74d1c645f0740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7533bbb825fb04690e5d0daaa82fdc32",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "d53839e2699056ef3c51",
+    "url": "/todoList/static/css/main.a2bbf513.chunk.css"
+  },
+  {
+    "revision": "72f855b3ef9ac023c9c6",
+    "url": "/todoList/static/js/2.237d6d95.chunk.js"
+  },
+  {
+    "revision": "d53839e2699056ef3c51",
+    "url": "/todoList/static/js/main.f2eb5de1.chunk.js"
+  },
+  {
+    "revision": "461a7cc0da04bf86a244",
+    "url": "/todoList/static/js/runtime-main.48dca3a1.js"
+  }
+]);
